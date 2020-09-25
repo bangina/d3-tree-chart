44106382f8f1f90203a258decc5c5fa48cc01ed0
ina <bbangina@gmail.com>
2020-09-25 22:29:05 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import TreeChart from "./TreeChart";
+import "./App.css";
+const initialData = {
+  name: "😐",
+  children: [
+    {
+      name: "🙂",
+      children: [
+        {
+          name: "😀",
+        },
+        {
+          name: "😁",
+        },
+        {
+          name: "🤣",
+        },
+      ],
+    },
+    {
+      name: "😔",
+    },
+  ],
+};
 
 function App() {
+  const [data, setData] = useState(initialData);
+  // const videoRef = useRef();
+  // useEffect(() => {
+  //   navigator.mediaDevices
+  //     .getUserMedia({ video: true, audio: false })
+  //     .then((stream) => {
+  //       videoRef.current.srcObject = stream;
+  //       videoRef.current.play();
+  //     });
+  // }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Animated Tree Chart</h1>
+      <TreeChart data={data} />
+      <button onClick={() => setData(initialData.children[0])}>
+        Update Data
+      </button>
+      {/* <video
+        ref={videoRef}
+        style={{ transform: "scale(-1,1)" }}
+        width="300"
+        height="150"
+      ></video> */}
+    </React.Fragment>
   );
 }
 
